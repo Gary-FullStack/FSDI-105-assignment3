dce234756be85eb312564456fc0e07170ae1c0e2
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-01-13 08:58:08 -0500

    initial commit
    assignment 3

---
+++ js/register.js
@@ -0,0 +1,53 @@
+
+function User(fName, lName, email, password, gender, age, address, phone, color) {
+	this.firstName = fName;
+	this.lastName = lName;
+	this.email = email;
+	this.password = password;
+	this.gender = gender;
+	this.age = age;
+	this.address = address;
+	this.phone = phone;
+	this.color = color;
+
+}
+
+function validation(user) {
+	let valid = true;
+	if (user.email == "") {
+		valid = false;
+		$("#txtEmail").addClass("alert-error");
+		$("#txtPassword").addClass("alert-error");
+	}
+	return valid;
+
+}
+
+let inputFName = $("#txtFname")
+let inputLName = $("#txtLname")
+let inputEmail = $("#txtEmail")
+let inputPassword = $("#txtPassword")
+let inputGender = $("#txtGender")
+let inputAge = $("#txtAge")
+let inputAddress = $("#txtAddress")
+let inputPhone = $("#txtPhone")
+let inputColor = $("#txtColor")
+
+
+function register() {
+
+	let newUser = new User(inputFName.val(), inputLName.val(), inputEmail.val(), inputPassword.val(), inputGender.val(), inputAge.val(), inputAddress.val(), inputPhone.val(), inputColor.val());
+
+	if (validation(newUser) == true) {
+		saveUser(newUser);
+		$("input").val("");
+	}
+
+}
+
+
+function init() {
+	$("#regbtn").click(register);
+}
+
+window.onload = init;
---
+++ js/users.js
@@ -0,0 +1,31 @@
+function displayUsers(usersArray) {
+    let tr = "";
+    for (let i = 0; i < usersArray.length; i++) {
+        let user = usersArray[i];
+        // console.log(user);
+
+        tr = `
+        <tr>
+            <td>${user.fName}</td>
+            <td>${user.lName}</td>
+            <td>${user.email}</td>
+            <td>${user.gender}</td>
+            <td>${user.age}</td>
+            <td>${user.phone}</td>
+            <td>${user.address}</td>
+            <td>${user.pay}</td>
+            <td>${user.color}</td>
+        </tr>`;
+
+        $("#usersTable").append(tr);
+    }
+}
+
+function init() {
+    console.log("Listing users");
+    let users = readUsers();
+    // console.log(users);
+    displayUsers(users);
+}
+
+window.onload = init;
